8639b639003e3f129c7988c7af4397a282a5bdaa
Patrick Hochstenbach <Patrick.Hochstenbach@UGent.be>
2022-06-12 13:12:41 +0200

    Adding bundle

--- gh-pages.js
+++ gh-pages.js
@@ -4,7 +4,7 @@ ghpages.publish(
 	'public', // path to public directory
 	{
 		branch: 'gh-pages',
-		repo: 'git@github.com:phochste/FormViewer.git', // Update to point to your repository
+		repo: 'git@github.com:phochste/SolidEditor.git', // Update to point to your repository
 		user: {
 			name: 'Patrick Hochstenbach', // update to use your name
 			email: 'patrick.hochstenbach@ugent.be' // Update to use your email
